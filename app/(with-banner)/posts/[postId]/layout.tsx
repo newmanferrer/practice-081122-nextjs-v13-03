a3329aa33a48e81012be07a0e17d9cca43537c65
Newman Ferrer <newmanferrer@gmail.com>
2022-11-17 14:35:04 -0400

    feat: :sparkles: change layout for "PostDetailsCard"
    change layout for "PostDetailsCard"

--- app/(with-banner)/posts/[postId]/layout.tsx
+++ app/(with-banner)/posts/[postId]/layout.tsx
@@ -1,11 +1,19 @@
+import { PostDetailsCard } from '../../../../components'
+
 interface IPostPageProps {
+  params: {
+    postId: string
+  }
   children: React.ReactNode
 }
 
-export default async function PostLayout({ children }: IPostPageProps) {
+export default async function PostLayout({ params, children }: IPostPageProps) {
+  const { postId } = params
+
   return (
     <>
-      <small>Posts Page &bull; Post Details Page</small>
+      <small>Posts Page &bull; Post Details Page and Comments</small>
+      <PostDetailsCard postId={postId} />
       {children}
     </>
   )
